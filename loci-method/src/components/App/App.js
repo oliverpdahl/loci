51a43e184b333176d5e0e89fa5df16e355398d46
Oliver Dahl <oliverpdahl@gmail.com>
2021-02-07 14:28:02 +0000

    state

--- loci-method/src/components/App/App.js
+++ loci-method/src/components/App/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import './App.css';
 import Dashboard from '../Dashboard/Dashboard';
+import Login from '../Login/Login';
 import Preferences from '../Preferences/Preferences';
 
 function App() {
+  const [token, setToken] = useState();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
     <div className="wrapper">
       <h1>Application</h1>
