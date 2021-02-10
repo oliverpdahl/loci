428464157c72932b21004b1ce88c83cf430366de
Oliver Dahl <oliverpdahl@gmail.com>
2021-02-10 23:10:15 +0000

    Pre authentication

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,6 +5,8 @@ import Dashboard from '../Dashboard/Dashboard';
 import Login from '../Login/Login';
 import Preferences from '../Preferences/Preferences';
 import useToken from './useToken';
+import {Navbar, Nav} from 'react-bootstrap'
+import {LinkContainer} from 'react-router-bootstrap'
 
 function App() {
 
@@ -12,14 +14,37 @@ function App() {
 
   return (
     <div className="wrapper">
-      <h1>Application</h1>
+      
       <BrowserRouter>
+      <Navbar bg='primary' variant='dark'>
+        <Navbar.Brand href="#home">The Loci Method</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Nav className="mr-auto">
+          <LinkContainer to="/dashboard">
+            <Nav.Link>Dashboard</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/preferences">
+            <Nav.Link>Preferences</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/login">
+            <Nav.Link>Login</Nav.Link>
+          </LinkContainer>
+          </Nav>
+          <Navbar.Text>
+            Signed in as: <a href="#login">Test User</a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Navbar>
         <Switch>
           <Route path="/dashboard">
             <Dashboard />
           </Route>
           <Route path="/preferences">
             <Preferences />
+          </Route>
+          <Route path="/login">
+            <Login />
           </Route>
         </Switch>
       </BrowserRouter>
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron'
 
 export default function Dashboard() {
   return(
-    <h2>Dashboard</h2>
+    <div>
+      <h2>Dashboard</h2>
+      <Jumbotron>
+        <h1>The Method of Loci</h1>
+        <h3>Create Mind Palaces to Remember Like Never Before</h3>
+        <p>asdf asdf asd fas df asd fas df asdf asd fa sdf as dfa sdf as dfas df</p>
+      </Jumbotron>
+    </div>
   );
 }
