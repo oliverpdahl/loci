8b0c6ed73bf330bc2c88e1ca347c3a1198c67510
Oliver Dahl <oliverpdahl@gmail.com>
2021-02-24 23:49:26 +0000

    Form Changed

--- src/components/Journey/JourneyContainer.js
+++ src/components/Journey/JourneyContainer.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import {Table} from 'react-bootstrap'
+import JourneyForm from './JourneyForm'
 
 export default function JourneyContainer() {
     return (
-        <div>
-            
-        </div>
+        <Table bordered hover>
+            <JourneyForm />
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                </tr>
+            </tbody>
+            </Table>
     )
 }
--- src/components/Journey/JourneyForm.js
+++ src/components/Journey/JourneyForm.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import {database} from  "../../firebase"
+import {Button, InputGroup, FormControl} from 'react-bootstrap'
 
 export default function JourneyForm() {
     const [title, setTitle] = useState('')
@@ -18,9 +19,23 @@ export default function JourneyForm() {
         journeyRef.push(journey)
     }
     return (
-        <div>
-            <input type="text" onChange={handleOnChange} value={title} />
-            <button onClick={createJourney}>Add Journey</button>
-        </div>
+        <thead>
+            <tr>
+            <th>#</th>
+            <td colSpan="2">
+                <InputGroup className="">
+                    <InputGroup.Prepend>
+                    <InputGroup.Text id="inputGroup-sizing-default">Title</InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <FormControl
+                    aria-label="title"
+                    aria-describedby="inputGroup-sizing-default"
+                    type="text" onChange={handleOnChange} value={title}
+                    />
+                </InputGroup>
+            </td>
+            <th><Button onClick={createJourney} block>Add Journey</Button></th>
+            </tr>
+        </thead>
     )
 }
